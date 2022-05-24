6497c5131d6605c4b1d18acb85b383e5fbe3e56d
AzadTom <kumarazad2917@gmail.com>
2022-05-24 17:45:08 +0530

    multiply.java added

---
+++ multiply.java
@@ -0,0 +1,16 @@
+package com.azad;
+
+public class multiply {
+
+    public static void main(String[]args)
+    {
+        int a=10,b=30;
+        int mul =a*b;
+
+        System.out.println("multiply :- "+ mul);
+        
+
+
+
+    }
+}
--- sum.java
+++ sum.java
@@ -7,7 +7,7 @@ public class sum {
         int a =10,b=20;
         int sum =a+b;
 
-        System.out.println("sum :- "+ sum);
+        System.out.println("sum :-"+ sum);
 
 
 
