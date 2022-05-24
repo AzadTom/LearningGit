38c0417134d3aaf74f529a15ff626c3280560cb3
AzadTom <kumarazad2917@gmail.com>
2022-05-24 17:09:21 +0530

    sum.java added

---
+++ sum.java
@@ -0,0 +1,15 @@
+package com.azad;
+
+public class sum {
+
+    public static void main(String[]args){
+
+        int a =10,b=20;
+        int sum =a+b;
+
+        System.out.println("sum :- "+ sum);
+
+
+
+    }
+}
