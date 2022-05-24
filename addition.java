dc43c0d4e00e77e98b05d6f49623332705a71add
AzadTom <kumarazad2917@gmail.com>
2022-05-24 17:13:54 +0530

    addition.java add

---
+++ addition.java
@@ -0,0 +1,14 @@
+package com.azad;
+
+public class addition {
+
+    public static void main(String[]args)
+    {
+        int a=10,b=20;
+
+        int add =a+b;
+        System.out.println("Addition :-  "+add);
+        
+
+    }
+}
